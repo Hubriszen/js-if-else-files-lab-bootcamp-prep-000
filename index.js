403fe06959d41e2e1152fafa2e02a164e2b9f74f
M H <ptcb795@gmail.com>
2018-08-27 01:44:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-var name="susan"
+var name="joe"
 var height = "74"
 
 
 // Don't worry about this
-module.exports = { name, height
-}
-if(name==="susan"){console.log("the name is correct")
-if(name==="Susan")}{console.log("the name is correct")}else{console.log("expeted the name to be susan")
